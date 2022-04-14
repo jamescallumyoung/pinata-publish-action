20c2204067c27bc9fc9210727b7ce0d08b0f3d40
James Young <hello@jamesyoung.ch>
2022-04-15 00:58:43 +0200

    feat: various updates

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
-import core from '@actions/core';
+import * as core from '@actions/core';
 import pinataSDK from '@pinata/sdk';
 
 (async function iife() {
   try {
-    const apiKey = core.getInput('pinata-api-key');
-    const apiSecret = core.getInput('pinata-secret-api-key');
-    const fileToPin = core.getInput('file-to-pin');
+    console.log('Reading inputs...');
+    const inputOptions: core.InputOptions = { required: false, trimWhitespace: false };
+    const apiKey = core.getInput('pinata-api-key', inputOptions);
+    const apiSecret = core.getInput('pinata-secret-api-key', inputOptions);
+    const fileToPin = core.getInput('file-to-pin', inputOptions);
 
+    console.log('Connecting to Pinata...');
     const pinata = pinataSDK(apiKey, apiSecret);
 
     // Test connection. Throws if authentication fails.
+    console.log('Testing connection...');
     await pinata.testAuthentication();
 
     const pinResult = await pinata.pinFromFS(fileToPin);
 
+    console.log('Setting outputs...');
     core.setOutput("hash", pinResult.IpfsHash);
     core.setOutput("pinSize", pinResult.PinSize);
     core.setOutput("timestamp", pinResult.Timestamp);
+
+    console.log('Done!');
   }
   catch (error: any) {
-    core.setFailed(error?.message);
+    console.log('... an error occurred in this step.');
+    core.setFailed(JSON.stringify(error));
   }
 })();
